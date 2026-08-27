2ba1d379739dec18edb7c75664b86964c24a6b7b
Vhien <vgranadil@gbox.adnu.edu.ph>
2026-08-27 14:39:50 +0800

    Add Subject scaffold and teacher relationship

---
+++ app/controllers/subjects_controller.rb
@@ -0,0 +1,70 @@
+class SubjectsController < ApplicationController
+  before_action :set_subject, only: %i[ show edit update destroy ]
+
+  # GET /subjects or /subjects.json
+  def index
+    @subjects = Subject.all
+  end
+
+  # GET /subjects/1 or /subjects/1.json
+  def show
+  end
+
+  # GET /subjects/new
+  def new
+    @subject = Subject.new
+  end
+
+  # GET /subjects/1/edit
+  def edit
+  end
+
+  # POST /subjects or /subjects.json
+  def create
+    @subject = Subject.new(subject_params)
+
+    respond_to do |format|
+      if @subject.save
+        format.html { redirect_to @subject, notice: "Subject was successfully created." }
+        format.json { render :show, status: :created, location: @subject }
+      else
+        format.html { render :new, status: :unprocessable_content }
+        format.json { render json: @subject.errors, status: :unprocessable_content }
+      end
+    end
+  end
+
+  # PATCH/PUT /subjects/1 or /subjects/1.json
+  def update
+    respond_to do |format|
+      if @subject.update(subject_params)
+        format.html { redirect_to @subject, notice: "Subject was successfully updated.", status: :see_other }
+        format.json { render :show, status: :ok, location: @subject }
+      else
+        format.html { render :edit, status: :unprocessable_content }
+        format.json { render json: @subject.errors, status: :unprocessable_content }
+      end
+    end
+  end
+
+  # DELETE /subjects/1 or /subjects/1.json
+  def destroy
+    @subject.destroy!
+
+    respond_to do |format|
+      format.html { redirect_to subjects_path, notice: "Subject was successfully destroyed.", status: :see_other }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_subject
+      @subject = Subject.find(params.expect(:id))
+    end
+
+    # Only allow a list of trusted parameters through.
+    def subject_params
+      params.fetch(:subject, {})
+    end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_08_22_023905) do
+ActiveRecord::Schema[8.1].define(version: 2026_08_27_060323) do
   create_table "departments", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.string "location"
@@ -34,6 +34,11 @@ ActiveRecord::Schema[8.1].define(version: 2026_08_22_023905) do
     t.string "program"
     t.datetime "updated_at", null: false
     t.integer "year_level"
+  end
+
+  create_table "subjects", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "teachers", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
