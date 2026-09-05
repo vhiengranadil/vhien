4e5c76c21b03f6e801a3b05894edf4d009291c42
Vhien <vgranadil@gbox.adnu.edu.ph>
2026-09-05 15:15:34 +0800

    [issue/7] Display list of subjects in student/show

--- app/models/section.rb
+++ app/models/section.rb
@@ -1,4 +1,5 @@
 class Section < ApplicationRecord
   belongs_to :subject
-  has_many :class_list, dependent: :destroy 
+  has_many :class_lists, dependent: :destroy 
+  has_many :students, through: :class_lists
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,4 +1,6 @@
 class Student < ApplicationRecord
     belongs_to :department
-    has_many :class_list, dependent: :destroy
+    
+    has_many :class_lists, dependent: :destroy
+    has_many :sections, through: :class_lists  #edited 
 end
